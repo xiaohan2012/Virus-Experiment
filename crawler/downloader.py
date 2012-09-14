bbd72b9e1f16f3e7e589482f179961a096692544
xiaohan <xiaohan2012@gmail.com>
2012-09-14 17:34:07 +0800

    download from pdbbank almost finished(beautiful code,hehe)

--- crawler/downloader.py
+++ crawler/downloader.py
@@ -1,1 +1,136 @@
 from urllib2 import urlopen
+from codecs import open
+from collections import defaultdict
+
+from threading import Thread
+from Queue import Queue
+
+    
+class DownloadThread(Thread):
+    def __init__(self,queue):
+        Thread.__init__(self)
+        self.q = queue
+
+    def _download(self,url,save_path,data={},headers={}):
+        f = open(save_path,"w","utf8").write(urlopen(url).read())
+        f.close()
+        return True
+
+    def run(self):
+        while True:
+            url,save_path= self.q.get()
+            self._download(url,save_path)
+            self.q.task_done()
+
+class BatchDownloader():
+    def __init__(self,url_path_list = [],thread_count = 10):
+        self.thread_count = thread_count
+        self.q = Queue()
+
+        for url,path in url_path_list:
+            self.q.put((url,path))
+
+        self.is_started = False
+
+    def addTasks(self,url_path_list):
+        """task can be added halfway"""
+        for url,path in url_path_list:
+            self.q.put((url,path))
+                    
+    def start(self):
+        """start downloading"""
+        self.is_started = True
+        self.thread_list = [DownloadThread(self.q) for i in xrange(self.thread_count)]
+        for thread in self.thread_list:
+            thread.setDaemon(True)
+            thread.start()
+        self.q.join()
+
+    def isStarted(self):
+        return self.is_started
+    
+
+                
+class PageInformationFetcher(Thread):
+    """fetch the download link contained in the webpage
+    """
+    def __init__(self,page_class , task_q , basket = [] , interested_info = {}):
+        Thread.__init__(self)
+        self.page_class = page_class
+        self.basket = basket
+        self.task_q = task_q
+        self.intersted_info = interested_info
+
+    def run(self):
+        while True:
+            page_url = self.task_q.get()
+
+            page = self.page_class(page_url)
+            print page_url
+            page.prepare(page_url)
+
+            for info_name,func_name in interested_info.items():
+                info = getattr(page,func_name)()
+                self.basket[info_name].append(info)
+            #url  =page.getDownloadUrl()#fetch the url
+
+            #self.url_basket.append(url)#put it in the basket
+
+class BatchPageInformationFetcher(Thread):
+    def __init__(self,page_class , page_urls=[],fetcher_count = 5,interested_info = {}):
+        self.task_q = Queue()
+        self.fetcher_count = fetcher_count
+        for url in page_urls:
+            self.task_q.put(url)
+
+        self.page_class = page_class#the class that represents the page where interested information hides in
+        
+        self.basket = defaultdict(list)#container to hold the information
+
+        self.is_start = False#indicate whether fetching has begun
+        self.interested_info = interested_info #a dict hold, the interested info names and corresponding fetching method
+
+    def isStarted(self):
+        return self.is_start
+
+    def addPageUrls(self,page_urls):
+        for url in page_urls:
+            self.task_q.put(url)
+        
+    def fetch(self):
+        self.fetchers = [PageInformationFetcher(self.page_class , self.task_q , self.basket , self.interested_info)\
+                             for i in xrange(self.fetcher_count)]
+        
+        for f in self.fetchers:
+            f.setDaemon(True)
+            f.start()
+    def getAllInformation(self):
+        self.task_q.join()
+        return self.url_basket
+
+if __name__ == "__main__":
+    #prepare the information fetcher
+    from pdbbank_crawler import SearchResultPage,ItemPage,load_page_content
+
+    interested_info = {
+        "download_url":"getDownloadUrl",
+        "pdb_id":"getPDBId",
+        "description":"getDescription",
+        }
+    info_fetcher = BatchPageInformationFetcher(ItemPage,interested_info = interested_info)
+
+    url_template = "http://www.rcsb.org/pdb/results/results.do?gotopage=%d&qrid=4D35F462&tabtoshow=Current"
+
+    srp = SearchResultPage(load_page_content(url_template %1))
+    srp.prepare(url_template)
+
+    page_urls = srp.getItemUrls()
+    info_fetcher.addPageUrls( page_urls )
+    info_fetcher.fetch()
+
+    while srp.gotoNextPage():
+        page_urls = srp.getItemUrls()[:2]
+        info_fetcher.addPageUrls( page_urls )
+        break
+    info = info_fetcher.getAllInformation() 
+    print info
--- crawler/pdbbank_crawler.py
+++ crawler/pdbbank_crawler.py
@@ -1,58 +1,122 @@
-from error import NoMoreNextError
+import re
 from pyquery import PyQuery as pq
 
+from urllib2 import urlopen , Request
 from urlparse import urlsplit, urljoin
+from collections import defaultdict
 
-class BankItemPage(pq):
-    def __init__(self,*args , **kwargs):
-        pq.__init__(self,*args , **kwargs)
+from error import NoMoreNextError
+
+def load_page_content(url):
+    headers = {
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Charset":"ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+            "Accept-Language":"en-US,en;q=0.8",
+            "Cache-Control":"max-age=0",
+            "Connection":"keep-alive",
+            "Cookie":"JSESSIONID=DB6E894842DAFECC4FF1899C8392D822; __utma=230211871.948318794.1347599384.1347599384.1347599384.1; __utmb=230211871.2.10.1347599384; __utmc=230211871; __utmz=230211871.1347599384.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)",
+            "Host":"www.rcsb.org",
+            "User-Agent":"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.79 Safari/537.1",
+        }
+    req = Request(url , headers = headers)     
+    res = urlopen(req)
+    return res.read()
     
+class ItemPage(pq):
+    def __init__(self,*args, **kwargs):
+        pq.__init__(self , *args , **kwargs)#load the page
+    
+    def prepare(self,url):
+        res = urlsplit(url)
+        self.url = url
+        self.baseurl = "%s://%s" %(res.scheme , res.netloc)
+
     def getPDBId(self):
-        return
-    
-    def getChainName(self):
-        return
-    
-    def getDescription(self):
-        return
+        return re.findall(r'structureId=(\w+)',self.url)[0]
 
+    def getDescription(self):
+        key_col = self("#widget_se_unitMDAU .contentNB").find("td.mdauCategory")
+        value_col = self("#widget_se_unitMDAU .contentNB").find("td.mdauData")
+        desc = defaultdict(dict)
+        for key , value in zip(key_col , value_col):
+            key = pq(key).text().lower().strip(":")
+            value = pq(value).text().strip()
+            if key == "molecule":
+                cur_molecule = value
+            else:                
+                desc[cur_molecule][key]=value
+        return desc            
+    
+    def getDownloadUrl(self):
+        return self._prepare_url(pq(self("#se_downloadFiles").find("a:contains('PDB File (Text)')")).attr("href"))
+
+    def _prepare_url(self , url):
+        """prepend the url with the baseurl"""
+        return urljoin(self.baseurl , url)
 class SearchResultPage(pq):
     def __init__(self,*args , **kwargs):
-        url = args[0]
-        res = urlsplit(url)
-        self.baseurl = "%s://%s" %(res.scheme , res.netloc)
-        print self.baseurl
         pq.__init__(self,*args , **kwargs)
-    
-    def getItemUrls(self):
-        for a in self.find("a.qrb_structid"):
-            print a
-#print pq(a).text(), urljoin(self.baseurl , pq(a).attr("href"))
-#yield  pq(a).text(), urljoin(self.baseurl , pq(a).attr("href"))
+        self.page_index = 1
+
+    def prepare(self,url_template):
+        """
+        set the url property using a separate function
+        pquery is somewhat odd, args and kwargs should be used in the __init__ function
+        """
+        res = urlsplit(url_template)
+        self.baseurl = "%s://%s" %(res.scheme , res.netloc)
+
+        self.url_template = url_template 
+
+        self._determine_last_page_index()
+
+    def _determine_last_page_index(self):
+        """anaylze the page and find/set the last page index"""
+        tds = pq(self(".lb_head3")[-1]).find("td")
+        last_url = pq(pq(tds[-1]).find("a")[-1]).attr("href")
+        self.last_index = int(re.findall(r"gotopage=(\d+)" , last_url)[0])
+
+
+    def _prepare_url(self , url):
+        """prepend the url with the baseurl"""
+        return urljoin(self.baseurl , url)
 
     def _get_next_url(self):
-        return
+        if self.page_index + 1 <= self.last_index:
+            self.page_index += 1
+            return self.url_template %self.page_index
+        else:
+            return ''            
 
-    def _get_prev_url(self):
-        return
+    def getItemUrls(self):
+        return [self._prepare_url( pq(a).attr("href"))  for a in self.find("a.qrb_structid")]
+
 
     def gotoNextPage(self):
         url = self._get_next_url()
         if url:
-            pq.__init__(self,url)
+            print url
+            pq.__init__(self,load_page_content(url))
+            return True
         else:
-            raise NoMoreNextError
-    
-    def gotoPrevPage(self):        
-        url = self._get_prev_url()
-        if url:
-            pq.__init__(self,url)
-        else:
-            raise NoMorePrevError
-                
+            return False            
+
+                    
+        
 if __name__ == "__main__":
-    search_result_url = "http://www.rcsb.org/pdb/results/results.do?qrid=10F3C09A&tabtoshow=Current"
-    srp = SearchResultPage(search_result_url)
     from pprint import pprint
-    pprint( srp.getItemUrls() )
-            
+    url_template = "http://www.rcsb.org/pdb/results/results.do?gotopage=%d&qrid=4D35F462&tabtoshow=Current"
+
+    srp = SearchResultPage(load_page_content(url_template %1))
+    srp.prepare(url_template)
+
+    urls = srp.getItemUrls()
+    while srp.gotoNextPage():
+        srp.getItemUrls()
+    
+    #pdb_url = "http://www.rcsb.org/pdb/explore/explore.do?structureId=1BJ1"
+    #item = ItemPage(pdb_url)
+    #item.prepare(pdb_url)
+    #print item.getPDBId()
+    #print item.getDescription()
+    #print item.getDownloadUrl()
