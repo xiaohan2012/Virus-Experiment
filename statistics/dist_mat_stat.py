7dea8bd602247be44df67e60990b0a2404ae0358
Xiao Han <xiaohan2012@gmail.com>
2012-09-21 17:32:20 +0800

    I don't want to plot through ssh connection\!So sync to my own PC

--- statistics/dist_mat_stat.py
+++ statistics/dist_mat_stat.py
@@ -1,10 +1,13 @@
 from nltk import FreqDist
 from sim_mat import load_sim_mat
+import math
 
 def get_freq_dist(matrix , step=0.05):
-    for r in matrix:
-        print r
+    return FreqDist(math.floor(c/step) * step for r in matrix for c in r)
+            
+
 
 if __name__ == "__main__":
     mat = load_sim_mat("WILM950103_dist_mat" )
-    get_freq_dist(mat)
+    fd = get_freq_dist(mat)
+    fd.tabulate([0.0,0.25,0.5,0.75,1.0])
