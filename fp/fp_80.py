708126cfc8c16080a72a583db132b4c5d8d37753
Xiao Han <xiaohan2012@gmail.com>
2013-03-01 00:28:01 +0800

    draft done: finger print, 175 bits, dual, 80 bits, dual

--- fp/fp_80.py
+++ fp/fp_80.py
@@ -7,6 +7,7 @@ from ve.util.dist import ResDistCache,FP105DistCache
 from residue_util import init_resdist_util, init_neighbour_util, init_axial_plane_util, init_electric_fp_util
 from complex_util import init_split_cylinder_util,init_triangle_util
 from complex import TriangleComplex
+from res_triangle import ResTriangle
 
 from ve.config import *
 
@@ -23,7 +24,16 @@ class Residue(BaseResidue):
 
         init_electric_fp_util(self)
         
-
+    def gen_last30_fp(self, others):
+        first = others[0]
+        if isinstance(first, ResTriangle):
+            return self.gen_last30_fp_triangle(others)
+        elif isinstance(first, Residue):
+            return self.gen_last30_fp_residue(others)
+        else:
+            raise TypeError("Only ResTriangle and Residue is ok.")
+       
+            
     def gen_last30_fp_triangle(self,others):
         self.gen_electric_fp_triangle(others)
         return self.fp
@@ -32,32 +42,29 @@ class Residue(BaseResidue):
         self.gen_electric_fp(others)
         return self.fp
 
-class Complex(TriangleComplex):
-    def __init__(self,complex_id, antigen, antibody):
+class GenericComplex(TriangleComplex):
+    def __init__(self,complex_id, antigen, antibody, atg_params_wrapper, atb_params_wrapper):
         TriangleComplex.__init__(self,complex_id, antigen,antibody)
         
         init_split_cylinder_util(self)
 
-        self.atg_param = dict(name="antigen",
-                         bases=self.atg.residues,
-                         targets=[(0,self.triangles)])
-        self.atb_param = dict(name="antibody",
-                         bases=self.atb.residues,
-                         targets=[(0,self.atb.residues)])
+        self.atg_param_50, self.atg_param_30 = atg_params_wrapper()#use wrapper to delay expression evaluation
+        self.atb_param_50, self.atb_param_30 = atb_params_wrapper()
+
         self.set_atg_atb_center()
 
         self.distcache = FP105DistCache()
 
     def gen_antigen_fp(self):
-        fp = self.get_fp_generic(** self.atg_param)
+        fp = self.get_fp_generic(** self.atg_param_50)
         for r in fp.residues():
-            fp[r].append(r.gen_last30_fp_triangle(self.triangles))
+            fp[r].append(r.gen_last30_fp(self.atg_param_30))
         return fp
         
     def gen_antibody_fp(self):
-        fp = self.get_fp_generic(** self.atb_param)
+        fp = self.get_fp_generic(** self.atb_param_50)
         for r in fp.residues():
-            fp[r].append(r.gen_last30_fp_residue(self.atb.residues))
+            fp[r].append(r.gen_last30_fp(self.atb_param_30))
         return fp
 
     def set_atg_atb_center(self):
@@ -73,6 +80,36 @@ class Complex(TriangleComplex):
         for r in self.atb.residues:
             r.set_axial_plane(self.atb_center)
 
+class ComplexSingle(GenericComplex):
+    def __init__(self,complex_id, antigen, antibody):
+        GenericComplex.__init__(self,complex_id, antigen, antibody,
+                                lambda :
+                                    (dict(name="antigen",
+                                          bases=self.atg.residues,
+                                          targets=[(0,self.triangles)]),
+                                     
+                                     self.triangles),
+                                lambda :
+                                    (dict(name="antibody",
+                                          bases=self.atb.residues,
+                                          targets=[(0,self.atb.residues)]),
+                                     self.atb.residues))
+
+class ComplexDual(GenericComplex):
+    def __init__(self,complex_id, antigen, antibody):
+        GenericComplex.__init__(self,complex_id, antigen, antibody,
+                                lambda :
+                                    (dict(name="antigen",
+                                          bases=self.atg.residues,
+                                          targets=[(0,self.atb.residues)]),
+                                     self.atb.residues),
+                                lambda :
+                                    (dict(name="antibody",
+                                          bases=self.atb.residues,
+                                          targets=[(0,self.triangles)]),
+                                     self.triangles))
+
+    
 
 def single_test(complex_id):            
     data_dir = os.path.join(data237_complex_root,complex_id)
@@ -82,7 +119,7 @@ def single_test(complex_id):
     antigen = load_pdb_struct(os.path.join(data_dir,"antigen.pdb"),residue_cls = Residue)
     antibody = load_pdb_struct(os.path.join(data_dir,"antibody.pdb"),residue_cls = Residue)
 
-    c = Complex(antigen, antibody)
+    c = ComplexDual(complex_id, antigen, antibody)
     atg_fp = c.gen_antigen_fp()
     atb_fp = c.gen_antibody_fp()
     
