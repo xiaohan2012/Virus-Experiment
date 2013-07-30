3cb00f619041923a506d4b442238af3938f66169
xiaohan <xiaohan2012@gmail.com>
2013-07-30 21:43:51 +0800

    added comparision for three types of similarity matrix

--- clustering/hcluster.py
+++ clustering/hcluster.py
@@ -5,9 +5,11 @@ from scipy.cluster.hierarchy import linkage,fcluster,dendrogram,fclusterdata
 import matplotlib.pyplot as plt
 
 from load_data import load_mat
-from categories import categorize, data
+from categories import categorize, data, data1
 
-def main():
+import numpy as np
+
+def compare():
 
     paths = ['CEpiMatch.csv', 'Multiprot.csv', 'SPa.csv', 'SPb.csv', 'SPe.csv', 'TMa.csv', 'TMb.csv', 'TMc.csv', 'MATT.csv']
 
@@ -15,6 +17,7 @@ def main():
         mats = categorize(load_mat("data/%s" %path), data)
         
         for name, mat in mats.items():
+            
             mat = 1 / mat
             
             Z = linkage(mat)
@@ -30,5 +33,53 @@ def main():
             plt.savefig("img/%s/%s.png" %( name, type_name))
 
 
+def main():
+    mat = load_mat("data/CEpiMatch.csv")
+    mat = 1 / mat
+    
+    Z = linkage(mat)
+    
+    #plt.figure()
+
+    dendrogram(Z, labels=mat.rlabels, orientation="right", leaf_font_size=5)
+
+    plt.title("Hierarchical clustering result")
+    plt.ylabel("PDB ID")
+    plt.plot()
+    plt.show()
+    #plt.savefig("img/166.png")
+
+def for_lysozyme():
+
+    import sys
+    method = sys.argv[1]
+    
+    paths = ['CEpiMatch.csv', 'Multiprot.csv', 'SPa.csv', 'SPb.csv', 'SPe.csv', 'TMa.csv', 'TMb.csv', 'TMc.csv', 'MATT.csv']
+
+    for path in paths:
+        mats = categorize(load_mat("data/%s" %path), data)
+        
+        mat = mats["lysozyme"]
+
+        if method == "upper":
+            mat = np.triu(mat)
+        elif method == "lower":
+            mat = np.transpose(np.tril(mat))
+        elif  method == "average":
+            mat = (np.tril(mat) + np.triu(mat)) / 2
+            
+                
+        plt.figure()
+        
+        Z = linkage(mat)
+        
+        dendrogram(Z, labels=mat.rlabels, orientation="right")
+        
+        plt.title("Hierarchical clustering for lysozyme")
+        plt.ylabel("PDB ID")
+        plt.savefig("img/for_lysozyme_%s/%s.png" %(method, path.split(".")[0]))
+
 if __name__ == '__main__':
-    main()
+    #main()
+    #compare()
+    for_lysozyme()
